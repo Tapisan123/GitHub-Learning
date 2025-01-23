bf5aeeac4abcfe37ac9758b5e3498a3b8042dec7
Santosh Rout <santosh.hello@gmail.com>
2025-01-23 10:13:11 -0800

    Add ListExample.py to demonstrate Python list functionality

---
+++ ListExample.py
@@ -0,0 +1,13 @@
+# to demonstrate python List example
+# List is a collection which is ordered and changeable. Allows duplicate members.
+# List is represented by square brackets []
+name = ["John", "Doe", "Jane", "Doe"]
+print(name)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[-1])
+print(name[-2])
+print(name[-3])
+print(name[-4])
